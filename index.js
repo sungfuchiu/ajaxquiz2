6c3c5dea532f0ce5c8377e508914e4aa8e8f71fd
sungfuchiu <sungfuchiu@gmail.com>
2023-07-26 14:55:04 -0700

    Update index to run the code upon every get request

--- index.js
+++ index.js
@@ -8,40 +8,47 @@ const PORT = process.env.PORT || 7000;
 
 app.use(express.json());
 
-mongoose.connect(`${process.env.CONNECTION_STRING}Exam`, {
-  useNewUrlParser: true,
-});
-const quizSchema = new mongoose.Schema({
-  name: String,
-  sid: String,
-});
 
-const Quiz = mongoose.model("Quiz", quizSchema);
-const myQuiz = new Quiz({
-  name: "Sung-Fu Chiu",
-  sid: "300371200",
-});
-Quiz.insertMany([myQuiz])
-  .then(() => {
-    console.log("Successfully insert a quiz.");
-  })
-  .catch((err) => {
-    console.log(err);
+const examRecordSchema = new mongoose.Schema({
+    name: String,
+    sid: String,
   });
+  
+  const ExamRecord = mongoose.model("ExamRecord", examRecordSchema);
+  
 
-Quiz.find()
-  .then((quizzes) => {
-    quizzes.forEach((quiz) => {
-      console.log(`name: ${quiz.name}`);
-      console.log(`sid: ${quiz.sid}`);
-    });
-  })
-  .catch((err) => {
-    console.log(err);
-    mongoose.connection.close();
-  }).finally(() => {
-    mongoose.connection.close();
-  })
+app.get("/", (req, res) => {
+  mongoose.connect(`${process.env.CONNECTION_STRING}/Exams23002`, {
+    useNewUrlParser: true,
+  });
+  let myExamRecord = new ExamRecord({
+    name: "Sung-Fu Chiu",
+    sid: "300371200",
+  });
+  myExamRecord = new ExamRecord({
+    name: "Sung-Fu Chiu",
+    sid: "300371200",
+  });
+  ExamRecord.insertMany([myExamRecord])
+    .then(() => {
+      console.log("Successfully insert an exam record.");
+      ExamRecord.find()
+        .then((quizzes) => {
+          quizzes.forEach((quiz) => {
+            console.log(`name: ${quiz.name}`);
+            console.log(`sid: ${quiz.sid}`);
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+
+      res.send("Name and sid are sent to the database");
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
